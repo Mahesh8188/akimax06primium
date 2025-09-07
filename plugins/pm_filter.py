9194c4c1adc476563991e7c64fd0a4be141ee0ca
Mahesh8188 <ddbambhava8182@gmail.com>
2025-09-07 22:55:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -60,9 +60,10 @@ logger.setLevel(logging.ERROR)
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_search(client, message):
     # Premium movie check
-query = message.text.strip()
-if await check_premium_movie(query, message):
-    return  # agar premium hai to normal search skip
+    query = message.text.strip()
+    if await check_premium_movie(query, message):
+        return  # agar premium hai to normal search skip
+
     await mdb.update_top_messages(message.from_user.id, message.text)
     bot_id = client.me.id
     user_id = message.from_user.id
@@ -93,10 +94,9 @@ if await check_premium_movie(query, message):
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def group_search(client, message):
     # Premium movie check
-query = message.text.strip()
-if await check_premium_movie(query, message):
-    return  # agar premium hai to normal search skip
-    
+    query = message.text.strip()
+    if await check_premium_movie(query, message):
+        return  # agar premium hai to normal search skip 
     # await message.react(emoji=random.choice(REACTIONS))
     await mdb.update_top_messages(message.from_user.id, message.text)
     user_id = message.from_user.id if message.from_user else None
